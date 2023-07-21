5b7a4bc5fd65137dd9639d9f7324226cabccb2f7
Hans Bakker <hansmbakker+github@gmail.com>
2023-07-21 15:25:07 +0200

    Initial commit

---
+++ PickerAccessibilityBugRepro/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PickerAccessibilityBugRepro
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PickerAccessibilityBugRepro/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PickerAccessibilityBugRepro
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
